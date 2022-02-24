a0e303a54870684ef53e302e5a4695a695196c19
Ekki <ekki@plicht.de>
2022-02-24 19:38:09 +0100

    after clang-format applied

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,20 @@ PA10: USART1_RX (AF1)
 
 int main(void)
 {
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; //RCC ON
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // RCC ON
 
-    GPIOA->MODER |= GPIO_MODER_MODER5_0 | GPIO_MODER_MODER6_0; //mode out GPIO 5 and 6
+    GPIOA->MODER |= GPIO_MODER_MODER5_0 | GPIO_MODER_MODER6_0; // mode out GPIO 5 and 6
 
     GPIOA->OTYPER = 0;
     GPIOA->OSPEEDR = 0;
 
-    //UART CLock
+    // UART CLock
     RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
     // UART GPIO
     GPIOA->MODER |= GPIO_MODER_MODER9_1 | GPIO_MODER_MODER10_1; // Pin 9 und 10 Alternate function mode
     GPIOA->AFR[1] |= 0x0001 << GPIO_AFRH_AFSEL9_Pos; // Pin 9 AF1
-    GPIOA->AFR[1] |= 0x0001 << GPIO_AFRH_AFSEL10_Pos; //Pin 10 AF1
-    //UART Config
+    GPIOA->AFR[1] |= 0x0001 << GPIO_AFRH_AFSEL10_Pos; // Pin 10 AF1
+    // UART Config
     USART1->BRR = SystemCoreClock / 9600;
     USART1->CR1 |= USART_CR1_TE | USART_CR1_UE | USART_CR1_RE | USART_CR1_RXNEIE;
     NVIC_EnableIRQ(USART1_IRQn);
@@ -43,7 +43,7 @@ int main(void)
     TIM1->PSC = 16 - 1; // Prescaler, CK_INT/PSC+1=CK_CNT
     TIM1->ARR = 8 - 1; // Auto Reload Register = Limit/Start for Up/Down counter
     TIM1->CCR1 = 8 - 1; // Capture Compare Register 1
-    /* 
+    /*
     16, 2, 2 = 125 kHz
      8, 2, 2 = 250 kHz
      4, 2, 2 = 500 kHz usw.
@@ -59,7 +59,7 @@ int main(void)
     TIM1->CR1 |= TIM_CR1_CEN; // enable timer
 
     while (1) {
-        //blink led
+        // blink led
         GPIOA->ODR ^= GPIO_ODR_5;
 
         // delay current blink period
@@ -74,7 +74,7 @@ void USART1_IRQHandler(void)
 
     // did we receive a char?
     if (USART1->ISR & USART_ISR_RXNE) {
-        uint8_t rxchar = (uint8_t)(USART1->RDR); //read char, clear interrupt flag
+        uint8_t rxchar = (uint8_t)(USART1->RDR); // read char, clear interrupt flag
 
         if ((rxchar == 's') || (rxchar == 'S')) {
             blink_curr = blink_slow;
